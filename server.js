2ee4d6847b9a7a6de6a3a754f0a52048f1af1d98
Eric <outboundflavor@gmail.com>
2020-02-27 11:02:10 -0600

    first commit with server setup

---
+++ server.js
@@ -0,0 +1,47 @@
+/*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  DEPENDENCIES
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
+const express = require('express');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const app = express();
+const db = mongoose.connection;
+/*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  PORT
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
+//  Allow use of Heroku or local port, depending on environment
+const PORT = process.env.PORT || 3000;
+/*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  DATABASE
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
+//  How it connects to the database with Heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/' + 'TEMP-CHANGE-THIS';
+//  Connection to Mongo
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+//  Error handlers
+db.on('error', (err) => console.log(err.message + ' is Mongodb not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+//  open the connection to mongo
+db.on('open', () => {});
+/*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  MIDDLEWARE
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
+//  use public folder for static assets
+app.use(express.static('public'));
+//  populates req.body with parsed info from forms
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+//  use method override
+app.use(methodOverride('_method'));
+/*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  ROUTES
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
+//  localhost:3000
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+/*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  LISTENER
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
+app.listen(PORT, () => console.log('Listening...', PORT));
