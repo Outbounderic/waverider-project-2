0cde6de6332490bfd0eddd937b7e120d738280d8
Eric <outboundflavor@gmail.com>
2020-02-28 15:23:19 -0600

    finshed index and created show route

--- controllers/listings_controllers.js
+++ controllers/listings_controllers.js
@@ -7,7 +7,7 @@ const Listing = require('../models/listings.js');
 /*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
   ROUTES
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
-//  New
+//  New - This is the form for the shop listing
 listingRoute.get('/new', (req, res) => {
   res.render('listings/new.ejs');
 });
@@ -17,20 +17,30 @@ listingRoute.get('/new', (req, res) => {
 //  Delete
 
 //  Show
-
+listingRoute.get('/:id', (req, res) => {
+  Listing.findById(req.params.id, (err, foundListing) => {
+    res.render('listings/show.ejs', {
+      listing: foundListing
+    });
+  });
+});
 //  Update
 
-//  Create
+//  Create - This is for the shop listing
 listingRoute.post('/', (req, res) => {
   Listing.create(req.body, (error, createdListing) => {
-    res.send(createdListing);
+    res.redirect('/')
   });
 });
 
-//  Index
+//  Index - This will be the index of the shop page
 listingRoute.get('/', (req, res) => {
-  res.send('index')
-})
+  Listing.find({}, (error, allListings) => {
+    res.render('listings/index.ejs', {
+      listings: allListings
+    });
+  });
+});
 
 /*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
   EXPORT
