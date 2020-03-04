2ffb6123a5a19b19398555dd3c8a46e03ea4aa04
Eric <outboundflavor@gmail.com>
2020-03-04 13:56:12 -0600

    created more routes for services

--- controllers/services_controllers.js
+++ controllers/services_controllers.js
@@ -2,36 +2,56 @@
   DEPENDENCIES
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
 const servicesRoute = require('express').Router();
-const Services = require('../models/services.js');
+const Service = require('../models/services.js');
 
 /*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
   ROUTES
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
 //  New
-
+servicesRoute.get('/new', (req, res) => {
+  res.render('services/new.ejs');
+});
 
 //  Edit
 
 
 //  Delete
-
+servicesRoute.delete('/:id', (req, res) => {
+  Service.findByIdAndRemove(req.params.id, () => {
+    res.redirect('/services')
+  })
+})
 
 //  Seed
 
 
 //  Show
-
+servicesRoute.get('/:id', (req, res) => {
+  Service.findById(req.params.id, (err, foundService) => {
+    res.render('services/show.ejs', {
+      service: foundService
+    })
+  })
+})
 
 //  Update
 
 
-//  Create - This is for the shop listing
-
+//  Create
+servicesRoute.post('/', (req, res) => {
+  Service.create(req.body, (err, createdService) => {
+    res.redirect('/services')
+  })
+})
 
 //  Index - This will be the index of the shop page
 servicesRoute.get('/', (req, res) => {
-  res.render('services/index.ejs');
-});
+  Service.find({}, (err, foundServices) => {
+    res.render('services/index.ejs', {
+      services: foundServices
+    })
+  })
+})
 
 /*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
   EXPORT
--- models/services.js
+++ models/services.js
@@ -1,1 +1,14 @@
 
+const mongoose = require('mongoose');
+
+const serviceSchema = new mongoose.Schema({
+  title : { type: String, required: true },
+  spots : { type: Number, required: true },
+  image : { type: String, required: true },
+  description : { type: String },
+  cost : { type: Number, required: true }
+});
+
+const Service = mongoose.model('Service', serviceSchema);
+
+module.exports = Service;
